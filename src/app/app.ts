6a6286db44f9c0df285937fb47efdace866033ed
Victor Henrique de Camargo Nestali <victornestali@gmail.com>
2025-08-11 10:59:50 +0200

    feat: Add components on App.ts file

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,11 +1,13 @@
 import { Component, signal } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { Navbar } from "./components/navbar/navbar";
+import { PrimaryButton } from "./components/primary-button/primary-button";
+import { SecondaryButton } from './components/secondary-button/secondary-button';
 import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet, Navbar],
+  imports: [RouterOutlet, Navbar, PrimaryButton, SecondaryButton, CommonModule],
   templateUrl: './app.html',
   styleUrl: './app.css'
 })
