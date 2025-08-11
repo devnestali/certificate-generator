ff977bb4a1e368faa6155f613c9c9daee5275e7c
Victor Henrique de Camargo Nestali <victornestali@gmail.com>
2025-08-11 11:29:32 +0200

    feat: Creating certificate item list component

--- src/app/app.ts
+++ src/app/app.ts
@@ -4,10 +4,11 @@ import { Navbar } from "./components/navbar/navbar";
 import { PrimaryButton } from "./components/primary-button/primary-button";
 import { SecondaryButton } from './components/secondary-button/secondary-button';
 import { CommonModule } from '@angular/common';
+import { ItemCertificate } from "./components/item-certificate/item-certificate";
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet, Navbar, PrimaryButton, SecondaryButton, CommonModule],
+  imports: [RouterOutlet, Navbar, PrimaryButton, SecondaryButton, CommonModule, ItemCertificate],
   templateUrl: './app.html',
   styleUrl: './app.css'
 })
---
+++ src/app/components/item-certificate/item-certificate.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+import { SecondaryButton } from "../secondary-button/secondary-button";
+
+@Component({
+  selector: 'app-item-certificate',
+  imports: [SecondaryButton],
+  templateUrl: './item-certificate.html',
+  styleUrl: './item-certificate.css'
+})
+export class ItemCertificate {
+
+}
