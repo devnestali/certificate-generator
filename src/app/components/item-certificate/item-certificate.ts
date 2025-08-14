cd8d974f4fe4285a794dff231bd276eba01ea016
Victor Henrique de Camargo Nestali <victornestali@gmail.com>
2025-08-14 10:02:32 +0200

    feat: Using RouterLink to navigate between pages

--- src/app/components/item-certificate/item-certificate.ts
+++ src/app/components/item-certificate/item-certificate.ts
@@ -1,12 +1,13 @@
 import { Component } from '@angular/core';
 import { SecondaryButton } from "../secondary-button/secondary-button";
+import { RouterLink } from '@angular/router';
 
 @Component({
   selector: 'app-item-certificate',
-  imports: [SecondaryButton],
+  imports: [SecondaryButton, RouterLink],
   templateUrl: './item-certificate.html',
   styleUrl: './item-certificate.css'
 })
 export class ItemCertificate {
-
+  id: string = '6'
 }
