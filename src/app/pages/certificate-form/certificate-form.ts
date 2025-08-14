05949f7e12e9d28f689352056a3ca3f578b9ccb8
Victor Henrique de Camargo Nestali <victornestali@gmail.com>
2025-08-14 11:06:56 +0200

    feat: Using NgModel on Form

--- src/app/pages/certificate-form/certificate-form.ts
+++ src/app/pages/certificate-form/certificate-form.ts
@@ -1,13 +1,16 @@
 import { Component } from '@angular/core';
 import { SecondaryButton } from '../../components/secondary-button/secondary-button';
 import { PrimaryButton } from "../../components/primary-button/primary-button";
+import { FormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-certificate-form',
-  imports: [SecondaryButton, PrimaryButton],
+  imports: [SecondaryButton, PrimaryButton, FormsModule],
   templateUrl: './certificate-form.html',
   styleUrl: './certificate-form.css'
 })
 export class CertificateForm {
-
+  name: string = ''
+  activity: string = ''
+  activityList: string[] = ['Angular', 'React', 'C#']
 }
