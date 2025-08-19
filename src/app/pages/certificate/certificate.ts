d327fe760886e28c50b4d68560c33e3d2921a272
Victor Henrique de Camargo Nestali <victornestali@gmail.com>
2025-08-19 09:20:57 +0200

    feat: Creating downloadCertificate function on certificate page using html2canvas

--- src/app/pages/certificate/certificate.ts
+++ src/app/pages/certificate/certificate.ts
@@ -1,8 +1,9 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, ElementRef, OnInit, ViewChild } from '@angular/core';
 import { SecondaryButton } from "../../components/secondary-button/secondary-button";
 import { ActivatedRoute, RouterLink } from '@angular/router';
 import { CertificateService } from '../../services/certificate';
 import { CertificateProps } from '../../interfaces/certificate';
+import html2canvas from 'html2canvas';
 
 @Component({
   selector: 'app-certificate',
@@ -14,7 +15,24 @@ export class Certificate implements OnInit{
   id: string | null = null
   certificate: CertificateProps | undefined
 
+  @ViewChild('certificateContainer') certificateElement!: ElementRef
+
   constructor(private certificateService: CertificateService, private route: ActivatedRoute){}
+
+  downloadCertificate() {
+    if(this.certificate == undefined) {
+      return
+    }
+
+    html2canvas(this.certificateElement.nativeElement, { scale: 2 }).then(
+      canvas => {
+        const link = document.createElement('a')
+        link.href = canvas.toDataURL('image/png')
+        link.download = 'certificate'+ this.certificate?.name.replaceAll(' ', '_') +'.png'
+        link.click()
+      }
+    )
+  }
 
   ngOnInit(): void {
     this.route.paramMap.subscribe(params => {
